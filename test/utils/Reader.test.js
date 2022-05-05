52cda87b937e7f9515f4188b029565973abcea9e
SalinasAlejandro <dykeo.alejandro@gmail.com>
2022-05-05 18:06:24 -0500

    Prueba de fallo

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -4,7 +4,7 @@ describe("Test for Reader", () => {
 
     test("1. Obtain qty list of explorers", () => {
         const explorers = Reader.readJsonFile("test/explorers.test.json");
-        expect(explorers.length).toBe(15);
+        expect(explorers.length).toBe(16);
     });
 
 });
